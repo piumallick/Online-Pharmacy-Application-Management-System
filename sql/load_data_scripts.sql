df5ad04bf137c3251f71c02d71e3a8d7243af134
Piu Mallick <piu.mallick@outlook.com>
2019-11-25 22:45:40 -0500

    Update load_data_scripts.sql

--- sql/load_data_scripts.sql
+++ sql/load_data_scripts.sql
@@ -1,6 +1,6 @@
 
 /* Load Data script for STORE table */
-LOAD DATA INFILE '1store.csv' 
+LOAD DATA INFILE '/data/1store.csv' 
 INTO TABLE store 
 FIELDS TERMINATED BY ',' 
 ENCLOSED BY '"'
@@ -8,7 +8,7 @@ LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
 /* Load Data script for CUSTOMERS table */
-LOAD DATA INFILE '2customers.csv' 
+LOAD DATA INFILE '/data/2customers.csv' 
 INTO TABLE customers 
 FIELDS TERMINATED BY ',' 
 ENCLOSED BY '"'
@@ -16,7 +16,7 @@ LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
 /* Load Data script for STAFF table */
-LOAD DATA INFILE '3staff.csv' 
+LOAD DATA INFILE '/data/3staff.csv' 
 INTO TABLE staff 
 FIELDS TERMINATED BY ',' 
 ENCLOSED BY '"'
@@ -24,7 +24,7 @@ LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
 /* Load Data script for MEDICINE table */
-LOAD DATA INFILE '4medicine.csv' 
+LOAD DATA INFILE '/data/4medicine.csv' 
 INTO TABLE medicine 
 FIELDS TERMINATED BY ',' 
 ENCLOSED BY '"'
@@ -32,7 +32,7 @@ LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
 /* Load Data script for MEDICINE_CATEGORY table */
-LOAD DATA INFILE '5medicine_category.csv' 
+LOAD DATA INFILE '/data/5medicine_category.csv' 
 INTO TABLE medicine_category 
 FIELDS TERMINATED BY ',' 
 ENCLOSED BY '"'
@@ -40,7 +40,7 @@ LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
 /* Load Data script for SUPPLIER table */
-LOAD DATA INFILE '6supplier.csv' 
+LOAD DATA INFILE '/data/6supplier.csv' 
 INTO TABLE supplier 
 FIELDS TERMINATED BY ',' 
 ENCLOSED BY '"'
@@ -48,7 +48,7 @@ LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
 /* Load Data script for STORE table */
-LOAD DATA INFILE '7stock.csv' 
+LOAD DATA INFILE '/data/7stock.csv' 
 INTO TABLE stock 
 FIELDS TERMINATED BY ',' 
 ENCLOSED BY '"'
@@ -56,7 +56,7 @@ LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
 /* Load Data script for HAS_STOCK_SUPPLY table */
-LOAD DATA INFILE '8has_stock_supply.csv' 
+LOAD DATA INFILE '/data/8has_stock_supply.csv' 
 INTO TABLE has_stock_supply 
 FIELDS TERMINATED BY ',' 
 ENCLOSED BY '"'
@@ -64,7 +64,7 @@ LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
 /* Load Data script for HAS_STORE_STOCK table */
-LOAD DATA INFILE '9has_store_stock.csv' 
+LOAD DATA INFILE '/data/9has_store_stock.csv' 
 INTO TABLE has_store_stock 
 FIELDS TERMINATED BY ',' 
 ENCLOSED BY '"'
@@ -72,7 +72,7 @@ LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
 /* Load Data script for ORDERS table */
-LOAD DATA INFILE '10orders.csv' 
+LOAD DATA INFILE '/data/10orders.csv' 
 INTO TABLE orders 
 FIELDS TERMINATED BY ',' 
 ENCLOSED BY '"'
@@ -80,7 +80,7 @@ LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
 /* Load Data script for ORDER_ITEMS table */
-LOAD DATA INFILE '11order_items.csv' 
+LOAD DATA INFILE '/data/11order_items.csv' 
 INTO TABLE order_items 
 FIELDS TERMINATED BY ',' 
 ENCLOSED BY '"'
